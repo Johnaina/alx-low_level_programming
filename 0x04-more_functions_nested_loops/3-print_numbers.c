3b7e8f6f0386cdc43e03766e37f9995239450874
John Aina <johnaina2013@gmail.com>
2022-07-08 21:40:11 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int 1;
+
+		i = 0;
+
+	while (i < 10)
+	{
+		_putchar (1 + '0');
+		i++;
+	}
+
+	_putchar ('\n');
+}
